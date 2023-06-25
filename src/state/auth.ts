5b126e2efe0896d2c4e7979fc85ea8de31331296
Hantsouski <Yury_Hantsouski@epam.com>
2023-06-25 23:09:32 +0400

    Import AuthorizationUpdates from Telegram into AuthState

--- src/state/auth.ts
+++ src/state/auth.ts
@@ -1,14 +1,32 @@
-import { fromView, syncRAF } from '@thi.ng/rstream';
+import { CloseMode, fromView, syncRAF } from '@thi.ng/rstream';
 import { DB } from './root';
-import { map } from '@thi.ng/transducers';
+import { distinct, map, trace } from '@thi.ng/transducers';
 import { AuthorizationStates } from './api';
+import telegram from '../data-access/telegram/telegram';
 
 export const auth = syncRAF(
   fromView(DB, {
     path: ['auth'],
   }),
-);
+  {
+    id: "authState",
+    closeOut: CloseMode.NEVER,
+  }
+).transform(distinct(), trace('auth STATE: '));
 
 export const isAuthorized = auth.transform(
   map(state => state === AuthorizationStates.Ready),
+  distinct(),
 );
+
+export const waitingPhoneNumber = auth.transform(
+  map(state => state === AuthorizationStates.WaitPhoneNumber),
+);
+
+const setAuthState = (authState: AuthorizationStates) => {
+  DB.resetIn(['auth'], authState);
+};
+
+telegram.auth().updates().on('any', update => {
+  setAuthState(update['@type'] as AuthorizationStates);
+})
