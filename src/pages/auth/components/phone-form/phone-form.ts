4098bbfc59ecc90c17c4acae9c24c565737bad2c
Hantsouski <Yury_Hantsouski@epam.com>
2023-07-01 16:51:00 +0400

    Create phoneForm component

---
+++ src/pages/auth/components/phone-form/phone-form.ts
@@ -0,0 +1,35 @@
+import { button, form, h1, inputText } from '@thi.ng/hiccup-html';
+import { centerL, logo, note, stackL } from '../../../../components';
+import { reactive } from '@thi.ng/rstream';
+import { comp, filter, map } from '@thi.ng/transducers';
+import { sendPhone } from '../../../../state';
+import { $input, $inputTrigger, $replace } from '@thi.ng/rdom';
+
+const phone = reactive('');
+const formSubmit = reactive(false);
+
+formSubmit.transform(
+  comp(
+    filter(Boolean),
+    map(() => sendPhone(phone.deref()!))
+  )
+);
+
+// dumb phone validation
+// will change later
+const submitButton = $replace(
+  phone.map((x) => (
+    x.length > 7
+      ? button({ onclick: $inputTrigger(formSubmit), type: 'button' }, 'NEXT')
+      : null
+  ))
+);
+
+export const phoneForm = stackL(
+  {},
+  centerL(null, logo),
+  h1(null, 'Telegram'),
+  note({}, 'Please confirm your country code and enter your phone number.'),
+  form({ id: 'phone-form' }, stackL(null, inputText({ id: 'phone', value: phone, oninput: $input(phone), minlength: 7 }), submitButton)),
+  button({ class: 'primary-text' }, 'Log in by QR code')
+);
