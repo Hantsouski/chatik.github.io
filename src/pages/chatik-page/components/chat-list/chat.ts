75a2369ba48377c4cd32a3251b24e03e20900c8b
Hantsouski <Yury_Hantsouski@epam.com>
2023-07-03 21:03:25 +0400

    Include more information in chat component

--- src/pages/chatik-page/components/chat-list/chat.ts
+++ src/pages/chatik-page/components/chat-list/chat.ts
@@ -1,6 +1,7 @@
-import { h4, anchor, div } from '@thi.ng/hiccup-html';
+import { h4, anchor, div, span } from '@thi.ng/hiccup-html';
 import { boxL, sideBarL, stackL, circle } from '../../../../components';
-import { Chat } from '../../../../state';
+import { Chat, isBasicGroup, isPhotoContent, isTextContent } from '../../../../state';
+import { abbreviatedName, croppedText, messageTime } from '../../../../common';
 
 import './chat.css';
 
@@ -10,13 +11,54 @@ export const chat = (chat: Chat) => (
     boxL(
       {},
       sideBarL(
-        { noStretch: true },
-        div({}, circle(chat.title)),
+        {},
+        div(
+          { class: 'avatar-container' },
+          circle({ name: abbreviatedName(chat.title), width: '54px', height: '54px', color: 'orange' }),
+        ),
         stackL(
           {},
-          h4({}, chat.title),
-        )
-      )
+          sideBarL(
+            { side: 'right' },
+            h4({}, chat.title),
+            messageTime(chat.last_message),
+          ),
+          sideBarL(
+            { side: 'right' },
+            span(
+              { class: 'chat-last-message'},
+              lastMessage(chat),
+            ),
+            unreadCount(chat),
+          ),
+        ),
+      ),
     ),
   )
 );
+
+const lastMessage = (chat: Chat) => {
+  const message = chat.last_message;
+
+  if (isTextContent(message.content)) {
+    return isBasicGroup(chat.type)
+      ? `Username: ${croppedText(message.content.text)}`
+      : croppedText(message.content.text);
+  }
+
+  if (isPhotoContent(message.content)) {
+    return message.media_album_id !== '0'
+      ? 'Album'
+      : (message.content.caption.text && croppedText(message.content.caption)) || 'Photo'
+  }
+
+  return 'Unsupported';
+};
+
+const unreadCount = (chat: Chat) => {
+  if (!chat.unread_count) {
+    return null;
+  }
+
+  return circle({ name: `${chat.unread_count}`, width: '30px', height: '30px', color: 'var(--accent-ui)' });
+}
