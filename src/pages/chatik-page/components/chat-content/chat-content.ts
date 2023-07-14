02d6b76f77ca1dfb1972a4ff9e6c6129cdca1a81
Hantsouski <Yury_Hantsouski@epam.com>
2023-07-14 22:51:10 +0400

    Update chat-content to support reselecting chats

--- src/pages/chatik-page/components/chat-content/chat-content.ts
+++ src/pages/chatik-page/components/chat-content/chat-content.ts
@@ -4,20 +4,20 @@ import { Chat, selectedChat } from '../../../../state';
 
 import './chat-content.css';
 import { $replace } from '@thi.ng/rdom';
-import { centerL } from '../../../../components';
+import { boxL, centerL } from '../../../../components';
 import { privateChat } from '../private-chat/private-chat';
 import { superGroupChat } from '../supergroup-chat/supergroup-chat';
 import { basicGroupChat } from '../basicgroup-chat/basicgroup-chat';
 
 const messages = selectedChat.map(chat => chat
-    ? centerL({ max: '60ch', andText: true }, messagesList(chat))
+    ? boxL({ class: 'messages-container', borderWidth: '0' }, centerL({ max: '60ch', class: 'chat-content' }, messagesList(chat)))
     : null
 );
 
 const messagesList = defmulti((chat: Chat) => chat.type['@type'], {}, {
-  'chatTypePrivate': () => privateChat,
-  'chatTypeSupergroup': () => superGroupChat,
-  'chatTypeBasicGroup': () => basicGroupChat,
+  'chatTypePrivate': () => privateChat(),
+  'chatTypeSupergroup': () => superGroupChat(),
+  'chatTypeBasicGroup': () => basicGroupChat(),
 });
 
 export const chatContent = $replace(messages);
