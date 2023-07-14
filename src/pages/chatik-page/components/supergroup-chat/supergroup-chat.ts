05105ed21153e4027a0725cee94f5edabf4ee369
Hantsouski <Yury_Hantsouski@epam.com>
2023-07-14 22:49:05 +0400

    Create super-group component

--- src/pages/chatik-page/components/supergroup-chat/supergroup-chat.ts
+++ src/pages/chatik-page/components/supergroup-chat/supergroup-chat.ts
@@ -1,3 +1,39 @@
-import { h2 } from '@thi.ng/hiccup-html';
+import { $klist } from '@thi.ng/rdom';
+import { groupedMessages } from '../../../../state';
+import { boxL, stackL } from '../../../../components';
+import { bubble } from '../../../../components/bubble/bubble';
+import { message as messageC } from '../message/message';
+import { day } from '../day/day';
 
-export const superGroupChat = h2({}, 'Supergroup');
+export const superGroupChat = () => {
+  return $klist(
+    groupedMessages,
+    'days-l',
+    { space: '1.5rem', reverse: true, role: 'list' },
+    (groupedByDay) => (
+      stackL(
+        { space: '1.5rem', reverse: true },
+        ...groupedByDay.map(groupedBySender => {
+
+          return stackL(
+            { space: '0.4rem', reverse: true },
+            ...groupedBySender.map((groupedByAlbum) => {
+              if (groupedByAlbum.length > 1) {
+                return bubble({}, boxL({ borderWidth: '0' }, 'Album content'));
+              }
+
+              const [ message ] = groupedByAlbum;
+
+              return stackL(
+                { reverse: true },
+                bubble({}, messageC(message))
+              );
+            }),
+          )
+        }),
+        day(groupedByDay[0][0][0].date),
+      )
+    ),
+    days => days.flatMap(senders => senders.map(messages => messages[0].id)).join(''),
+  );
+};
