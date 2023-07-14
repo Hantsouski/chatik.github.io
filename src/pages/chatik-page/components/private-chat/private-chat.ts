f18635771f9029f6fd4564a695f7502b1034f740
Hantsouski <Yury_Hantsouski@epam.com>
2023-07-14 22:48:50 +0400

    Create private-chat component

--- src/pages/chatik-page/components/private-chat/private-chat.ts
+++ src/pages/chatik-page/components/private-chat/private-chat.ts
@@ -1,3 +1,48 @@
-import { h2 } from '@thi.ng/hiccup-html';
+import { $klist } from '@thi.ng/rdom';
+import { groupedMessages, isMe, isPhotoContent } from '../../../../state';
+import { boxL, stackL } from '../../../../components';
+import { bubble } from '../../../../components/bubble/bubble';
+import { message as messageC } from '../message/message';
+import { day } from '../day/day';
 
-export const privateChat = h2({}, 'Private');
+export const privateChat = () => {
+  return $klist(
+    groupedMessages,
+    'days-l',
+    { space: '1.5rem', reverse: true, role: 'list' },
+    (groupedByDay) => (
+      stackL(
+        { space: '1.5rem', reverse: true },
+        ...groupedByDay.map(groupedBySender => {
+          const me = isMe('user_id' in groupedBySender[0][0].sender_id
+              ? groupedBySender[0][0].sender_id.user_id
+              : groupedBySender[0][0].sender_id.chat_id);
+
+          const align = me ? 'flex-end': '';
+
+          return stackL(
+            { space: '0.4rem', reverse: true, align },
+            ...groupedBySender.map((groupedByAlbum, index) => {
+              if (groupedByAlbum.length > 1) {
+                return bubble({}, boxL({ borderWidth: '0' }, 'Album content'));
+              }
+
+              const [ message ] = groupedByAlbum;
+
+              const straightCorner = index === 0 && !isPhotoContent(message.content)
+                ? me ? 'right' : 'left'
+                : undefined;
+
+              return stackL(
+                { reverse: true, align },
+                bubble({ straightCorner }, messageC(message))
+              );
+            }),
+          )
+        }),
+        day(groupedByDay[0][0][0].date),
+      )
+    ),
+    days => days.flatMap(senders => senders.map(messages => messages[0].id)).join(''),
+  );
+};
