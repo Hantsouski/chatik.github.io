de295df887a0b7ce52ae456a9abaececc4c80d2a
Hantsouski <Yury_Hantsouski@epam.com>
2023-07-15 16:16:26 +0400

    Add intersectionObserver to watch reaching the top of a chat to fetch more messages

--- src/pages/chatik-page/components/chat-content/chat-content.ts
+++ src/pages/chatik-page/components/chat-content/chat-content.ts
@@ -1,16 +1,25 @@
+import { filter } from '@thi.ng/transducers';
+import { div } from '@thi.ng/hiccup-html';
+import { $replace } from '@thi.ng/rdom';
 import { defmulti } from '@thi.ng/defmulti';
 
-import { Chat, selectedChat } from '../../../../state';
-
 import './chat-content.css';
-import { $replace } from '@thi.ng/rdom';
+
 import { boxL, centerL } from '../../../../components';
+import { Chat, fetchMore, selectedChat } from '../../../../state';
 import { privateChat } from '../private-chat/private-chat';
 import { superGroupChat } from '../supergroup-chat/supergroup-chat';
 import { basicGroupChat } from '../basicgroup-chat/basicgroup-chat';
 
 const messages = selectedChat.map(chat => chat
-    ? boxL({ class: 'messages-container', borderWidth: '0' }, centerL({ max: '60ch', class: 'chat-content' }, messagesList(chat)))
+    ? boxL(
+        { class: 'chat-content-container', borderWidth: '0' },
+        centerL(
+          { max: '60ch', class: 'chat-content' },
+          messagesList(chat),
+        ),
+        div({ id: 'top' }),
+      )
     : null
 );
 
@@ -19,5 +28,24 @@ const messagesList = defmulti((chat: Chat) => chat.type['@type'], {}, {
   'chatTypeSupergroup': () => superGroupChat(),
   'chatTypeBasicGroup': () => basicGroupChat(),
 });
+
+const reachingTopCallback = (entries: any) => {
+  const [ entry ] = entries;
+
+  if (entry.isIntersecting) {
+    fetchMore();
+  }
+}
+
+messages.transform(filter(Boolean)).subscribe(({ next: () => {
+  requestAnimationFrame(() => {
+    const top = document.getElementById('top')!;
+    const root = document.querySelector('.chat-content-container')!;
+
+    const observer = new IntersectionObserver(reachingTopCallback, { root, rootMargin: '1500px' });
+
+    observer.observe(top);
+  });
+}}));
 
 export const chatContent = $replace(messages);
